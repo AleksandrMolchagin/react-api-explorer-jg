2fedfd8badda259108873b1c1b21877114a0b969
AleksandrMolchagin <gitnoreply@aleksandrmolchagin.com>
2025-06-13 09:25:56 -0400

    Init project

---
+++ app/welcome/welcome.tsx
@@ -0,0 +1,15 @@
+
+export function Welcome() {
+  return (
+    <main className="">
+        <header className="">
+          Header
+        </header>
+        <div className="">
+          <nav className="">
+
+          </nav>
+        </div>
+    </main>
+  );
+}
