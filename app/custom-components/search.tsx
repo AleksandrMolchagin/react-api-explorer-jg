bc676735d19be7a1ec7fc5ba8abdfd912159d1f0
AleksandrMolchagin <gitnoreply@aleksandrmolchagin.com>
2025-06-13 10:28:27 -0400

    Routing & simple home page

---
+++ app/custom-components/search.tsx
@@ -0,0 +1,9 @@
+import { Input } from "../components/ui/input"
+
+export function SearchBar() {
+  return (
+    <main className="">
+      <Input/>
+    </main>
+  );
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,31 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Input } from "../components/ui/input";
+import { Button } from "../components/ui/button"
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Meals Explorer" },
+    { name: "description", content: "Compile Your Cravings" },
   ];
 }
+//    <h1 className="text-4xl font-extrabold mb-4">Meals Explorer</h1>
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 gap-2">
+
+      <div className="flex items-center space-x-2 ">
+        <h3 className="text-2xl font-semibold">Compile Your Cravings</h3>
+        <img className="h-10" src="/food-tray.svg" alt="tray icon" />
+      </div>
+
+      <div className="w-5/6 max-w-md flex flex-col gap-2">
+        <Input placeholder="Find a recipe or ingredient..." />
+      </div>
+      <div className="flex gap-2">
+        <Button variant="outline" >Search</Button>
+        <Button variant="outline" >I am lucky today!</Button>
+      </div>
+    </div>
+  );
 }
